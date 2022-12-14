b02c83649257fbe6e9344abe05cbd4f28ea3f58c
Michael Maldonado <michael@maldonado.tech>
2022-12-14 14:52:34 -0600

    updates + keyboard

--- scripts/bulk-edit-obsidian.sh
+++ scripts/bulk-edit-obsidian.sh
@@ -6,13 +6,11 @@ do
     #list all matching files 
     echo "$f"
     #find and replace string with another string
-    #sed -i 's/Language/tool/g' "$f"
+    sed -i 's/#unixsec/[unix-security]/g' "$f"
     #prepend frontmatter
     #echo -e "---\nDate: 2022-11-10 \nTags: #unixsec\n---\n$(cat "$f")" > "$f" 
     #prepend frontmatter if missing
     #grep -xqF -- "---" "$f"
     # || echo -e "---\n---\n$(cat "$f")" > "$f" 
-    #move all to folder
-    mv $f "/home/punchy/Documents/main-vault/Unix Security/"
 done
 
