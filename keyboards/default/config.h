b4c8feb14c0f42f521d52d4e4ed043add400b32c
Michael Maldonado <michael@maldonado.tech>
2023-01-24 15:46:07 -0600

    keeb update add new layer

--- keyboards/default/config.h
+++ keyboards/default/config.h
@@ -3,7 +3,7 @@
 #ifdef TAPPING_TERM // TAPPING_TERM is set to 100 by default in the default sofle config.h file
 #undef TAPPING_TERM
 #endif
-#define TAPPING_TERM 200 //TAPPING_TERM is how long a tap is a tap, right now a tap is 200ms, tested with 100ms and that is too quick to be a tap on the sofle, 200 seems to be too slow and is causing accidental parenthesis
+#define TAPPING_TERM 175 //TAPPING_TERM is how long a tap is a tap, right now a tap is 200ms, tested with 100ms and that is too quick to be a tap on the sofle, 200 is too slow and is causing accidental parenthesis
 #define RETRO_TAPPING
 /* The way how "handedness" is decided (which half is which),
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
