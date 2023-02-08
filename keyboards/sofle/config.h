e122b15fcb0879827d526e836f7e5a37b7c5dc81
Michael Maldonado <michael@maldonado.tech>
2023-02-08 16:37:20 -0600

    added combos

--- keyboards/sofle/config.h
+++ keyboards/sofle/config.h
@@ -4,5 +4,7 @@
 #undef TAPPING_TERM
 #endif
 #define TAPPING_TERM 200 //TAPPING_TERM is how long a tap is a tap, right now a tap is 200ms, tested with 100ms and that is too quick to be a tap on the sofle, 175 is still too quick, 200 is too slow and is causing accidental parenthesis 200 seems to be the best
+#define TAPPING_TERM_PER_KEY
 #define RETRO_TAPPING
 #define USER_SUPER_ALT_TAB_TIMEOUT 500
+#define COMBO_COUNT 2
