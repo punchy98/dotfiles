2a24aa6d1f7c65f9adf8d9cf5aa808d93c297f54
Michael Maldonado <michael@maldonado.tech>
2023-02-28 10:01:11 -0600

    added symlink script

---
+++ scripts/links.sh
@@ -0,0 +1,7 @@
+ln -s dotfiles/bash/.bash_functions ~/.bash_functions
+ln -s dotfiles/bash/.bash_aliases ~/.bash_aliases
+ln -s dotfiles/bash/.bash_prompt ~/.bash_prompt
+ln -s dotfiles/bash/.bashrc ~/.bashrc
+ln -s dotfiles/git/.gitconfig ~/.gitconfig
+ln -s dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s dotfiles/vim/.vimrc ~/.vimrc
