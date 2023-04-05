4ba4889521abc50bdcad29182a6c14ead77e3a22
Michael Maldonado <michael@maldonado.tech>
2023-04-05 16:13:38 -0500

    reorg to fit qmk without forking or cloning

--- keyboards/default/keymap.c
+++ keyboards/default/keymap.c
@@ -39,19 +39,19 @@ enum combos {
   XC_COPY,
   CV_PASTE,
   XV_CUT,
-//  FJ_CLOSE,
+  XCV_LEFTCLK,
 };
 
 const uint16_t PROGMEM xc_combo[] = {KC_X, KC_C, COMBO_END};
 const uint16_t PROGMEM cv_combo[] = {KC_C, KC_V, COMBO_END};
 const uint16_t PROGMEM xv_combo[] = {KC_X, KC_V, COMBO_END};
-//const uint16_t PROGMEM fj_combo[] = {KC_F, KC_J, COMBO_END};
+const uint16_t PROGMEM xcv_combo[] = {KC_X, KC_C, KC_V, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   [XC_COPY] = COMBO(xc_combo, LCTL(KC_C)),
   [CV_PASTE] = COMBO(cv_combo, LCTL(KC_V)),
   [XV_CUT] = COMBO(xv_combo, LCTL(KC_X)),
- // [FJ_CLOSE] = COMBO(fj_combo, LCTL(KC_W)),
+  [XCV_LEFTCLK] = COMBO(xcv_combo, KC_BTN2),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
