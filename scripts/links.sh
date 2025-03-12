c981ed0839dd4fcc9cb9e2ac5aa3846f316ace02
Michael Maldonado <michael@maldonado.tech>
2025-03-11 23:24:22 -0500

    changed bashrc link script

--- scripts/links.sh
+++ scripts/links.sh
@@ -1,6 +1,7 @@
 ln -s dotfiles/bash/.bash_functions ~/.bash_functions
 ln -s dotfiles/bash/.bash_aliases ~/.bash_aliases
 ln -s dotfiles/bash/.bash_prompt ~/.bash_prompt
+rm ~/.bashrc
 ln -s dotfiles/bash/.bashrc ~/.bashrc
 ln -s dotfiles/git/.gitconfig ~/.gitconfig
 ln -s dotfiles/tmux/.tmux.conf ~/.tmux.conf
